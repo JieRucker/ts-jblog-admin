42a8ff99a2a8dd37f062bf776e681d05d112b861
JieRucker <1435255141@qq.com>
2019-02-20 18:12:49 +0800

    [fix] update tsconfig

--- src/modules/app/vuex/index.ts
+++ src/modules/app/vuex/index.ts
@@ -2,11 +2,10 @@
  * Created by wbt on 2018/2/20.
  */
 import Vue from 'vue';
-import Vuex from 'vuex';
+import * as Vuex from 'vuex';
 
 import app from './app/app';
 import user from './user/user';
-import async from './dynamic/async';
 import qrcode from './qrcode/qrcode';
 
 Vue.use(Vuex);
@@ -15,7 +14,6 @@ const store = new Vuex.Store({
   modules: {
     app,
     user,
-    async,
     qrcode,
   }
 });
